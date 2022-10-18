988ac0d706f14c1806fccb175e48534f4cb57b06
Bruno Borges <bruno.borges@gmail.com>
2022-10-18 23:42:28 +0000

    another ergonomic bug

--- test-ergonomics/test.sh
+++ test-ergonomics/test.sh
@@ -2,6 +2,11 @@
 
 cpus=${1:-1}
 memory=${2:-'128m'}
+heappercent=${3}
+
+if [ -n "$heappercent" ]; then
+    heappercent="-XX:InitialRAMPercentage=$heappercent -XX:MinRAMPercentage=$heappercent -XX:MaxRAMPercentage=$heappercent"
+fi
 
 echo "Starting evaluation with $cpus CPUs and $memory memory."
 
@@ -14,10 +19,12 @@ docker run --rm -v "$(pwd)":/usr/src/myapp -w /usr/src/myapp $image javac Hello.
 # See source: https://github.com/openjdk/jdk/blob/46e6e41b9a35c8665eb31be2f8c36bbdcc90564a/src/hotspot/share/runtime/arguments.cpp#L1745
 echo "# Default Heap Size:"
 docker run --memory=$memory --cpus=$cpus -v `pwd`:/app $image 2>/dev/null \
-    java -XX:+PrintFlagsFinal -cp /app Hello | grep 'MaxHeapSize'
+    java -XX:+AlwaysPreTouch $heappercent -XX:+PrintFlagsFinal -cp /app Hello | grep 'HeapSize\|RAM'
 
 # Test Default Garbage Collector
 echo "# Default Garbage Collector:"
 docker run --memory=$memory --cpus=$cpus -v `pwd`:/app $image 2>/dev/null \
     java -XX:+PrintFlagsFinal -cp /app Hello | grep 'UseSerial\|UseG1\|UseParallel'
 
+# Bugs:
+# - https://bugs.openjdk.org/browse/JDK-8278492
