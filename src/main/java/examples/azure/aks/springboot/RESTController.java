ea7800f5c6dd0d7e42c968a497db015e5f12d975
Bruno Borges <brborges@microsoft.com>
2025-04-09 20:20:28 -0700

    unused import

--- src/main/java/examples/azure/aks/springboot/RESTController.java
+++ src/main/java/examples/azure/aks/springboot/RESTController.java
@@ -15,7 +15,6 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
